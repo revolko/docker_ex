796ee98c3c4d416c296615fb2616c9db76b1d3da
Juraj Paluba <juraj.palubajp@gmail.com>
2025-12-21 22:05:12 +0100

    feat(client): create GET requests wrapper
    Maybe I could have used 3rd party lib for sending HTTP requests, but
    this was more fun.
    
    TODO:
     * handle Content-Length header
     * create post/delete/put? functions

---
+++ lib/docker_ex/client.ex
@@ -0,0 +1,65 @@
+defmodule DockerEx.Client do
+  @moduledoc """
+  Docker API client. Helper for sending requests to Docker Engine API.
+  """
+
+  require Logger
+
+  defstruct headers: []
+
+  def get(path) do
+    {:ok, socket} =
+      :gen_tcp.connect({:local, "/var/run/docker.sock"}, 0, [
+        :binary,
+        {:active, false},
+        {:packet, :http_bin}
+      ])
+
+    :gen_tcp.send(
+      socket,
+      "GET #{path} HTTP/1.1\nHost: #{:net_adm.localhost()}\n\n"
+    )
+
+    {:ok, body} = do_recv(socket)
+    Jason.decode(body)
+  end
+
+  defp do_recv(socket), do: do_recv(socket, :gen_tcp.recv(socket, 0), %DockerEx.Client{})
+
+  defp do_recv(socket, {:ok, {:http_response, {1, 1}, 200, "OK"}}, resp) do
+    do_recv(socket, :gen_tcp.recv(socket, 0), resp)
+  end
+
+  defp do_recv(socket, {:ok, {:http_header, _, header, _, value}}, resp) do
+    do_recv(socket, :gen_tcp.recv(socket, 0), %DockerEx.Client{resp | headers: [{header, value}]})
+  end
+
+  defp do_recv(socket, {:ok, :http_eoh}, resp) do
+    case :proplists.get_value(:"Transfer-Encoding", resp.headers) do
+      "chunked" -> read_chunked_body(socket, [])
+      :undefined -> {:error, "Not implemented -- Body is not chunked."}
+    end
+  end
+
+  defp read_chunked_body(socket, acc) do
+    :inet.setopts(socket, [{:packet, :line}])
+
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, chunk_length} ->
+        chunk_length = String.trim_trailing(chunk_length, "\r\n") |> String.to_integer(16)
+
+        if chunk_length == 0 do
+          {:ok, :erlang.iolist_to_binary(Enum.reverse(acc))}
+        else
+          :inet.setopts(socket, [{:packet, :raw}])
+          {:ok, data} = :gen_tcp.recv(socket, chunk_length)
+          # get rid of trailing \r\n
+          :gen_tcp.recv(socket, 2)
+          read_chunked_body(socket, [data | acc])
+        end
+
+      other ->
+        {:error, other}
+    end
+  end
+end
---
+++ lib/docker_ex/containers.ex
@@ -0,0 +1,5 @@
+defmodule DockerEx.Containers do
+  @moduledoc """
+  TODO
+  """
+end
