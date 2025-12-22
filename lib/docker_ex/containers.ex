db55ff888f1a817ae5fd6a1a070d4e97fd5363ba
Juraj Paluba <juraj.palubajp@gmail.com>
2025-12-22 23:10:34 +0100

    feat(containers): create get_containers function
    Getting query parameters will be moved to some "utils" module.

--- lib/docker_ex/containers.ex
+++ lib/docker_ex/containers.ex
@@ -1,5 +1,15 @@
 defmodule DockerEx.Containers do
   @moduledoc """
-  TODO
+  SDK for /containers endpoints
   """
+  alias DockerEx.Client
+
+  def get_containers(opts \\ []) do
+    query_parameters =
+      Keyword.keys(opts)
+      |> Enum.map(fn key -> "#{Atom.to_string(key)}=#{Keyword.get(opts, key)}" end)
+      |> Enum.join("&")
+
+    Client.get("/containers/json?#{query_parameters}")
+  end
 end
