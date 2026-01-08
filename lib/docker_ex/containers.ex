d39be835a21d18d1bcda65381f35e93980fff047
Juraj Paluba <juraj.palubajp@gmail.com>
2026-01-08 18:25:22 +0100

    feat(containers): wrap inspect container endpoint

--- lib/docker_ex/containers.ex
+++ lib/docker_ex/containers.ex
@@ -12,6 +12,12 @@ defmodule DockerEx.Containers do
     Client.get("/containers/json?#{query_parameters}")
   end
 
+  def inspect_container(id, opts \\ []) do
+    query_parameters = Utils.get_query_parameters(opts)
+
+    Client.get("/containers/#{id}/json?#{query_parameters}")
+  end
+
   def create_container(create_body = %CreateContainer{}, opts \\ []) do
     query_parameters = Utils.get_query_parameters(opts)
 
