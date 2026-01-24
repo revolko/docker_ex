30b6c4070430050ed654053de8a8941fa3c7e57b
Juraj Paluba <juraj.palubajp@gmail.com>
2026-01-24 22:46:26 +0100

    feat(images): create SDK for /images
    /images/create
    Ignore response body as it is an invalid JSON

---
+++ lib/docker_ex/images.ex
@@ -0,0 +1,47 @@
+defmodule DockerEx.Images do
+  @moduledoc """
+  SDK for /images endpoints
+  """
+
+  alias DockerEx.Client
+
+  def list_images(all \\ "false") do
+    Client.get("/images/json?all=#{all}")
+  end
+
+  @doc """
+  Builds Docker image that can be later used for creating container.
+
+  Currently, only bulding from remote (tested with GitHub repo) is supported.
+  The Dockerfile must be present in the root of the repo (remote cannot
+  handle relative paths for inner Dockerfiles).
+
+  ## Options
+
+   * `:remote_url` - URL to a repository with Dockerfile
+      The repository must be public since the authorization to a private
+      repository is not yet implemented.
+   * `:name` - The name of the image to be build
+   * `:tag` - The tag of the image. Default `tag` is "latest"
+
+  ## Examples
+
+    iex> DockerEx.Images.build_image("https://github.com/revolko/vegetation.git", "vegetation")
+    {:ok, ""}
+  """
+  def build_image(remote_url, name, tag \\ "latest") do
+    # build accepts only raw dockerfile or git repo url (ending with git)
+    # this struggles with inner Dockerfiles using relatives path inside the repo
+    Client.post(
+      "/build?remote=#{remote_url}&t=#{name}:#{tag}",
+      "",
+      ignore_response_body: true
+    )
+  end
+
+  def create_image(from_image, tag \\ "latest") do
+    Client.post("/images/create?fromImage=#{from_image}&tag=#{tag}", "",
+      ignore_response_body: true
+    )
+  end
+end
