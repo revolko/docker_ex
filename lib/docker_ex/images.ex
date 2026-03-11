b9bd81c6599564fa86596542b5a9898132563bfc
Juraj Paluba <juraj.palubajp@gmail.com>
2026-03-11 22:05:10 +0100

    feat(images)!: unify doc and options handling
    Regarding the function arguments and options:
      * arguments are query parameters that are mandatory
      * options are all the rest query parameters
    
    BREAKING CHANGE: Functions don't accepts arguments with default values
    but list of optionals. Moreover, changing `name` to `t` of the
    build_image function is done to follow the endpoint definition.
    
    ---v2
    Use URI.encode_query/2 to properly encode query parameters.
    
    ---v3
    Create encode_query Utils function that sanitizes the optional
    query parameters.

--- lib/docker_ex/images.ex
+++ lib/docker_ex/images.ex
@@ -4,38 +4,90 @@ defmodule DockerEx.Images do
   """
 
   alias DockerEx.Client
-
-  def list_images(all \\ "false") do
-    Client.get("/images/json?all=#{all}")
-  end
+  alias DockerEx.Utils
 
   @doc """
-  Builds Docker image that can be later used for creating container.
-
-  Currently, only bulding from remote (tested with GitHub repo) is supported.
-  The Dockerfile must be present in the root of the repo (remote cannot
-  handle relative paths for inner Dockerfiles).
+  Lists existing images.
 
   ## Options
 
-   * `:remote_url` - URL to a repository with Dockerfile
+  The options are optional query parameters of the `GET /images/json` endpoint.
+  See: `https://docs.docker.com/reference/api/engine/version/v1.53/#tag/Image/operation/ImageList`
+
+  ## Examples
+
+    iex> DockerEx.Images.list_images()
+    {:ok, [{ ... }]}
+
+  """
+  @doc since: "0.1.0"
+  def list_images(opts \\ []) do
+    query_parameters = Utils.encode_query([], opts)
+
+    Client.get("/images/json?#{query_parameters}")
+  end
+
+  @doc """
+  Builds Docker image from a GIT repository.
+
+  Currently, only building from public remote (tested with GitHub repo)
+  is supported. The Dockerfile must be present in the root of the repo
+  (remote cannot handle relative paths for inner Dockerfiles).
+
+  ## Parameters
+
+   * `:remote` - URL to a repository with Dockerfile
       The repository must be public since the authorization to a private
       repository is not yet implemented.
-   * `:name` - The name of the image to be build
-   * `:tag` - The tag of the image. Default `tag` is "latest"
+   * `:t` - The name of the image to be built.
+      Can be provided as `name:tag` or as `name`.
+      In the second case, the `tag` is `default`.
+
+  ## Options
+
+  The options are optional query parameters of the `POST /build` endpoint.
+  See: `https://docs.docker.com/reference/api/engine/version/v1.53/#tag/Image/operation/ImageBuild`
 
   ## Examples
 
     iex> DockerEx.Images.build_image("https://github.com/revolko/vegetation.git", "vegetation")
-    {:ok, ""}
+    {:ok, [{ ... }]}
+
   """
-  def build_image(remote_url, name, tag \\ "latest") do
+  @doc since: "0.1.0"
+  def build_image(remote, t, opts \\ []) do
+    query_parameters = Utils.encode_query([remote: remote, t: t], opts)
+
     # build accepts only raw dockerfile or git repo url (ending with git)
     # this struggles with inner Dockerfiles using relatives path inside the repo
-    Client.post_streamed("/build?remote=#{remote_url}&t=#{name}:#{tag}", "")
+    Client.post_streamed("/build?#{query_parameters}", "")
   end
 
-  def create_image(from_image, tag \\ "latest") do
-    Client.post_streamed("/images/create?fromImage=#{from_image}&tag=#{tag}", "")
+  @doc """
+  Pulls an image from a registry.
+
+  Currently, only default DockerHub registry is supported. The image
+  must be publicly available.
+
+  ## Parameters
+
+   * `from_image` - name of the image in the registry
+
+  ## Options
+
+  The options are optional query parameters of the `POST /images/create` endpoint.
+  See: `https://docs.docker.com/reference/api/engine/version/v1.53/#tag/Image/operation/ImageCreate`
+
+  ## Examples
+
+    iex> DockerEx.Images.create_image("fedora")
+    {:ok, [{ ... }]}
+
+  """
+  @doc since: "0.1.0"
+  def create_image(from_image, opts \\ []) do
+    query_parameters = Utils.encode_query([fromImage: from_image], opts)
+
+    Client.post_streamed("/images/create?#{query_parameters}", "")
   end
 end
