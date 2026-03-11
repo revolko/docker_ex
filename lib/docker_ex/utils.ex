015163167833cdbb19326d999768a1d3585375b8
Juraj Paluba <juraj.palubajp@gmail.com>
2026-03-11 22:05:10 +0100

    feat(images)!: unify doc and options handling
    Regarding the function arguments and options:
      * arguments are query parameters that are mandatory
      * options are all the rest query parameters
    
    BREAKING CHANGE: Functions don't accepts arguments with default values
    but list of optionals. Moreover, changing `name` to `t` of the
    build_image function is done to follow the endpoint definition.
    
    ---v2
    Use URI.encode_query/2 to properly encode query parameters.
    
    ---v3
    Create encode_query Utils function that sanitizes the optional
    query parameters.

--- lib/docker_ex/utils.ex
+++ lib/docker_ex/utils.ex
@@ -8,4 +8,27 @@ defmodule DockerEx.Utils do
     |> Enum.map(fn key -> "#{Atom.to_string(key)}=#{Keyword.get(opts, key)}" end)
     |> Enum.join("&")
   end
+
+  @doc """
+  Encodes parameters to a query string.
+
+  The function sanitizes `optional` parameters by dropping duplicates
+  defined in `mandatory`. That way users cannot redefine mandatory
+  query parameter in `optional` keyword list.
+
+  ## Parameters
+
+   * `mandatory` - keyword list of mandatory query parameters
+   * `optional` - keyword list of optional query parameters
+
+  ## Examples
+
+    iex> DockerEx.Utils.encode_query([from_image: "image_name"], [from_image: "not_image", size: 1])
+    "from_image=image_name&size=1"
+  """
+  @doc since: "0.1.0"
+  def encode_query(mandatory, optional) do
+    (mandatory ++ Keyword.drop(optional, Keyword.keys(mandatory)))
+    |> URI.encode_query(:rfc3986)
+  end
 end
