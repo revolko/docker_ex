02cc690d7dd2e1d4c56e402ddc70c600fb4ded1f
Juraj Paluba <juraj.palubajp@gmail.com>
2026-03-24 20:35:07 +0100

    build(doc): add ex_doc package
    Used by mix hex.publish to generate the documentation.

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule DockerEx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, "~> 1.4"}
+      {:jason, "~> 1.4"},
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false, warn_if_outdated: true}
     ]
   end
 end
